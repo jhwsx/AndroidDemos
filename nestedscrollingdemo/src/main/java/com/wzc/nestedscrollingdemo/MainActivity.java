9dcf12b0c44eb7f9fc1b0c83f46eb4f41f404ce9
wangzhichao <392337950@qq.com>
2017-07-23 19:03:58 +0800

    1,初始提交NestedScrollingDemo项目

---
+++ nestedscrollingdemo/src/main/java/com/wzc/nestedscrollingdemo/MainActivity.java
@@ -0,0 +1,14 @@
+package com.wzc.nestedscrollingdemo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+// 学习地址:
+// http://blog.csdn.net/al4fun/article/details/53888990
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
